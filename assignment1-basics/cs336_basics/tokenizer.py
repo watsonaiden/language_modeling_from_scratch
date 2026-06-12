514777bd570a13536d9974d61af104c166cb9b39
Aiden Watson <awats1819@gmail.com>
2026-06-11 18:19:34 -0700

    ⚡️: Speedup tokenization via heap and linked list
    Current speed is roughly .5 mb/s. Tiktoken is roughly 26x faster. This
    approach is roughly 43x faster than previous naive approach due to not
    checking merges that are not in the text and more optimized lookup of
    next valid merge via a heap.

--- assignment1-basics/cs336_basics/tokenizer.py
+++ assignment1-basics/cs336_basics/tokenizer.py
@@ -1,9 +1,37 @@
+from collections import defaultdict
+import heapq
 import regex as re
 
 from typing import Iterable, Iterator
 
 from cs336_basics.pre_tokenize import PAT
 from cs336_basics.bpe import BPEStats
+
+from dataclasses import dataclass
+
+
+@dataclass
+class Token:
+    # represent the paired bytes
+    next_token: "Token" = None
+    prev_token: "Token" = None
+    token: bytes = b""
+
+    def merge_next(self: "Token") -> "Token":
+        t2 = self.next_token
+        if t2 is None:
+            raise RuntimeError("called merge on end token", self)
+        # merge into one token
+        self.token = self.token + t2.token
+        self.next_token = t2.next_token
+        if t2.next_token is not None:
+            t2.next_token.prev_token = self
+
+        # wipe t2 so it's obvious its no longer active
+        # Without this it is hard to tell if a node is dangling since we may still reference due to lazy checking
+        t2.token, t2.next_token, t2.prev_token = b"", None, None
+
+        return self
 
 
 class Tokenizer:
@@ -14,7 +42,9 @@ class Tokenizer:
         self.special_tokens = special_tokens or []
         self.special_tokens.sort(key=len, reverse=True)
         assert isinstance(self.special_tokens, (type(None), list))
+
         self.merges = merges
+        self.merges_priority = {merge: count for count, merge in enumerate(merges)}
 
     @classmethod
     def from_files(cls, merges_file_path, vocab_file_path, special_tokens=None):
@@ -52,21 +82,63 @@ class Tokenizer:
         if text in self.special_tokens:
             return [self.inverted_vocab[text.encode()]]
 
-        pretoken_ids = [self.inverted_vocab[bytes([byte])] for byte in text.encode()]
+        # fake starter token for easier iteration
+        fake_starter = Token()
+        prev = fake_starter
 
-        for merge_pair in self.merges:
-            start, end = 0, 1
-            while end < len(pretoken_ids):
-                if (self.inverted_vocab[merge_pair[0]], self.inverted_vocab[merge_pair[1]]) == (
-                    pretoken_ids[start],
-                    pretoken_ids[end],
-                ):
-                    pretoken_ids[start : end + 1] = [self.inverted_vocab[merge_pair[0] + merge_pair[1]]]
+        for byte_value in text.encode():
+            t = Token(token=bytes([byte_value]), prev_token=prev)
+            prev.next_token = t
+            prev = t
 
-                start += 1
-                end += 1
+        start = fake_starter.next_token
 
-        return pretoken_ids
+        pairs = defaultdict(list)
+        heap = []
+        t1, t2 = start, start.next_token
+        while t2 is not None:
+            pair = (t1.token, t2.token)
+            pairs[pair].append(t1)
+            if pair in self.merges_priority:
+                heap.append((self.merges_priority[pair]))
+
+            t1 = t1.next_token
+            t2 = t2.next_token
+
+        # heap contains merge ids with earliest merge at the top
+        heapq.heapify(heap)
+        while heap:
+            top = heapq.heappop(heap)
+            expected_bytes = self.merges[top]
+
+            for first_token in pairs[expected_bytes]:
+                if first_token.next_token is None:
+                    continue
+                elif (first_token.token, first_token.next_token.token) != expected_bytes:
+                    continue
+
+                new_token = first_token.merge_next()
+
+                if new_token.prev_token is not None:
+                    pair = (new_token.prev_token.token, new_token.token)
+                    if pair in self.merges_priority:
+                        heapq.heappush(heap, self.merges_priority[pair])
+                        pairs[pair].append(new_token.prev_token)
+
+                if new_token.next_token is not None:
+                    pair = (new_token.token, new_token.next_token.token)
+                    if pair in self.merges_priority:
+                        heapq.heappush(heap, self.merges_priority[pair])
+                        pairs[pair].append(new_token)
+
+        output = []
+        # first "token" is just used as an anchor
+        fake_starter = fake_starter.next_token
+        while fake_starter:
+            output.append(self.inverted_vocab[fake_starter.token])
+            fake_starter = fake_starter.next_token
+
+        return output
 
     def encode_iterable(self, iterable: Iterable[str]) -> Iterator[int]:
         """
